52da2c66ea8930c49244345bdb44f468989b8b55
TBtuo <tuo.zhang@telecom-bretagne.eu>
2016-07-20 19:21:49 +0200

    the tests of javascripting

---
+++ arrays.js
@@ -0,0 +1,9 @@
+// var pets = ['cat', 'dog', 'rat'];
+
+var pizzaToppings = ['tomato sauce', 'cheese', 'pepperoni'];
+
+// for (var i in pizzaToppings) {
+//   if (pizzaToppings.hasOwnProperty(i))
+//     console.log(pizzaToppings);
+// }
+console.log(pizzaToppings);
